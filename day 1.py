c6230630fd659a4d719d1affd17d5c0346934391
DIYA BIJU <97796361+diyabijuthomas@users.noreply.github.com>
2025-09-01 12:40:00 +0530

    Update day 1.py

--- day 1.py
+++ day 1.py
@@ -1,4 +1,5 @@
 #1 Reverse a list - a[::-1]
+
 #2 
 There are 16 hourglasses in a 6*6 array. The hourglass sum  is the sum of the values in an hourglass. Calculate the hourglass sum for every hourglass in arr, then print the maximum  hourglass sum.
 
@@ -14,12 +15,14 @@ def hourglassSum(arr):
             if n[i][j]>max:
                 max=n[i][j]
     return max
-better approach
-def hourglassSum(arr):
-    max_hourglass = float('-inf')  # Handles negative sums
+    
+#better approach
 
-    for i in range(4):  # 0 to 3
-        for j in range(4):  # 0 to 3
+def hourglassSum(arr):
+    max_hourglass = float('-inf')  
+
+    for i in range(4):  
+        for j in range(4):  
             top = arr[i][j] + arr[i][j+1] + arr[i][j+2]
             middle = arr[i+1][j+1]
             bottom = arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
@@ -29,3 +32,7 @@ def hourglassSum(arr):
                 max_hourglass = hourglass
 
     return max_hourglass
+#note that u use float('-inf') to initiate max, since negative sum can be also max value
+
+
+
