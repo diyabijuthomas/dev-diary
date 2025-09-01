9282f25c67660a522f018dc6d48446e09599080e
DIYA BIJU <97796361+diyabijuthomas@users.noreply.github.com>
2025-09-01 15:45:36 +0530

    Update day 1.py

--- day 1.py
+++ day 1.py
@@ -63,3 +63,71 @@ def dynamicArray(n, queries):
 
 #major insight is that u can define the values in a list as a,b, c = i, effectively unpacking it
 
+#4
+#sort a list of dictionaries 
+a=[{"ravi":56},{"savu":80},{"Akhil":58}]
+#flatten the list of dictionaries into a single dictionary instead of list
+flat={k:v for d in a for k,v in d.items()}
+sorted_names=sorted(flat,key=flat.get)
+print(sorted_names)
+
+#OR
+scores={}
+for item in a:
+    for name, score in item.items()z:
+        scores[name]=score
+sorted_names=sorted(score,key=score.get)
+
+#5 array's elements 1  unit to the left. The elements that fall off the left end reappear at the right end. Given an integer d, rotate the array that many steps to the left and return the result.
+
+# STDIN      Function
+# -----      --------
+# 5 4         n = 5 d = 4
+# 1 2 3 4 5  arr = [1, 2, 3, 4, 5]
+# Sample Output
+
+# 5 1 2 3 4
+def rotateLeft(d, arr):
+    new=[]
+    new=arr[d:]
+    for i in range (d):
+        new.append(arr[i])
+    arr=new
+    return arr
+
+#6
+def matchingStrings(stringList, queries):
+    # Write your code here
+    result=[]
+    for query in queries:
+        s=0
+        for stri in stringList:
+            if stri==query:
+                s+=1
+        result.append(s)
+    return result
+
+#7 Timeout error is there
+# STDIN       Function
+# -----       --------
+# 5 3         arr[] size n = 5, queries[] size q = 3
+# 1 2 100     queries = [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
+# 2 5 100
+# 3 4 100
+# Sample Output
+
+# 200
+
+def arrayManipulation(n, queries):
+    arr=[0 for _ in range (n)]
+    for query in queries:
+        start,end,val=query
+        for i in range(start-1,end):
+            arr[i]+=val
+    for i in arr:
+        if i>high:
+            high=i
+    return high
+
+max(arr)- can also find the highest
+
